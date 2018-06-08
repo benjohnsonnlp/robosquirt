5691544b5a8d4f85b7a87a6e65adec18d64d8790
johnsolb <johnsolb@us.ibm.com>
2018-06-08 16:15:29 -0400

    bind to any address

--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,5 @@
 cd robosquirt
 python -m robosquirt.server > squirt.log &
 cd ../moistmaster/
-./manage.py runserver *:8080 2>&1 moist.log &
+./manage.py runserver 0.0.0.0:8080 2>&1 moist.log &
 cd ..
