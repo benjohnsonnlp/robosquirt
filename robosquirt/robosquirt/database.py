80ae3e7033b800fda5583c686e3e2c46a2bd532c
doc.ben.johnson@gmail.com <doc.ben.johnson@gmail.com>
2018-07-22 16:46:15 -0400

    more config changes (accounting for empty sections)

--- robosquirt/robosquirt/database.py
+++ robosquirt/robosquirt/database.py
@@ -1,11 +1,17 @@
+from configparser import NoSectionError
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from robosquirt.config import config
+from robosquirt.config import config, get_project_root
 
 
 def get_sqlite_file_connection_url():
-    return "sqlite:///{pth}/robosquirt.db".format(pth=config["sqlite_db_path"])
+    try:
+        path = config.get("database", "sqlite_db_path") or str(get_project_root())
+    except NoSectionError:
+        path = str(get_project_root())
+    return "sqlite:///{pth}/robosquirt.db".format(pth=path)
 
 
 def get_engine():
