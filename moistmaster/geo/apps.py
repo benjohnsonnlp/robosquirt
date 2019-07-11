28e644cd04db45d8d08c98e0b873134e7ba2224e
James Stevenson <james.m.stevenson@gmail.com>
2019-07-11 18:43:50 -0400

    Create user settings on app ready hook.

--- moistmaster/geo/apps.py
+++ moistmaster/geo/apps.py
@@ -1,5 +1,13 @@
+import logging
+
 from django.apps import AppConfig
 
 
 class GeoConfig(AppConfig):
+
+    logger = logging.getLogger("moistmaster")
     name = 'geo'
+
+    def ready(self):
+        from .models import UserSettings
+        UserSettings.objects.get_or_create_default()
